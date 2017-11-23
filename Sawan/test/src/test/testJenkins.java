4b42af8191b7e3435a2e5d2cdf9671a67893e5ce
swansingh.bora@smartdatainc.net <swansingh.bora@smartdatainc.net>
2017-11-23 20:45:07 +0530

    added text

---
+++ Sawan/test/src/test/testJenkins.java
@@ -0,0 +1,15 @@
+package test;
+
+public class testJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		while (i<10) {
+		System.out.println("Hello !");
+		i++;
+		}	
+//		adfasdfadf
+		//Santhosh
+	}
+	}
