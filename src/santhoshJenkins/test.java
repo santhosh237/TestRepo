7a76989736f5331c917d5ac5eced45675ed47913
Santosh Alavala <Santosh Alavala@DESKTOP-JC3M207>
2017-11-24 22:00:27 +0530

    added santhosh

---
+++ src/santhoshJenkins/test.java
@@ -0,0 +1,20 @@
+package santhoshJenkins;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		while (i<10) {
+		System.out.println("Hello !");
+		i++;
+		}	
+//		adfasdfadf
+		//Santhosh
+		//asdfasdf
+		//chandhu
+		//added newly
+		//asdfasdfSrikar
+		//addedSanthosh
+	}
+	}
